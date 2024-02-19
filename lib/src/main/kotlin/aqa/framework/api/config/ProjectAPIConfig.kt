499ffcfd0b7aa9f3d7b8ca39084baeba840c8d27
Dmitriy Malohlovets <dmalohlovets@tzero.com>
2024-02-19 22:14:20 +0200

    huge refactoring

--- lib/src/main/kotlin/aqa/framework/api/config/ProjectAPIConfig.kt
+++ lib/src/main/kotlin/aqa/framework/api/config/ProjectAPIConfig.kt
@@ -7,14 +7,11 @@ import io.restassured.filter.log.ResponseLoggingFilter
 import org.hamcrest.Matchers
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.ComponentScan
-import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.PropertySource
 import org.springframework.context.annotation.Scope
 
-@ComponentScan
-@Configuration
-@PropertySource("application-\${spring.profiles.active}.yml", factory = YamlPropertySource::class)
+//@ComponentScan
+//@Configuration
+//@PropertySource("application-\${spring.profiles.active}.yml", factory = YamlPropertySource::class)
 open class ProjectAPIConfig {
     @Value("\${app.url}")
     private lateinit var baseURL: String
@@ -24,12 +21,11 @@ open class ProjectAPIConfig {
 
     @Bean
     @Scope("singleton")
-    open fun getRestAssured(): RestAssured {
+    fun restAssuredConfiguration() {
         RestAssured.baseURI = baseURL
         RestAssured.filters(RequestLoggingFilter(), ResponseLoggingFilter())
         RestAssured.useRelaxedHTTPSValidation()
         RestAssured.responseSpecification =
             ResponseSpecBuilder().expectResponseTime(Matchers.lessThan(timeout.toLong())).build()
-        return RestAssured()
     }
 }
